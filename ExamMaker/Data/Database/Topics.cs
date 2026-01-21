978dad19b843615c4a7714006b700409789ed5f1
Yasin Erguen <yasin.erguen001@stud.fh-dortmund.de>
2026-01-21 15:21:36 +0100

    topics table added

---
+++ ExamMaker/Data/Database/Topics.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamMaker.Data.Database;
+
+
+[PrimaryKey(nameof(TopicId))]
+public class Topics
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+    public int TopicId { get; set; }
+    public string? TopicName { get; set; }
+}
